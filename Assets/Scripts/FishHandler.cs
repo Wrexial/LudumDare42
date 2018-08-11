9f45883d4eba03936eef78bfaaf3a3a8de668d83
Unknown <dylan.abela@gmail.com>
2018-08-11 21:08:32 +0200

    Setup for speed moding

--- Assets/Scripts/FishHandler.cs
+++ Assets/Scripts/FishHandler.cs
@@ -48,18 +48,19 @@ public class FishHandler : MonoBehaviour
     {
         while (Vector3.Distance(position, transform.localPosition) > DISTANCE_CUTOFF)
         {
-            transform.localPosition = Vector3.MoveTowards(transform.localPosition, position, Speed * Time.deltaTime);
-
-            Vector3 vectorToTarget = position - transform.localPosition;
-            float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
+            var thisSpeed = Speed;
+            var vectorToTarget = position - transform.localPosition;
+            var angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
 
             //if (WaterHandler.Instance.WaterEdgeOn)
             //{
             //    var position = WaterHandler.Instance.GetClosestToEdge(transform.localPosition);
-
+                  //Mod thisSpeed
             //}
 
-            Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+            transform.localPosition = Vector3.MoveTowards(transform.localPosition, position, thisSpeed * Time.deltaTime);
+
+            var q = Quaternion.AngleAxis(angle, Vector3.forward);
             transform.rotation = Quaternion.Slerp(transform.rotation, q, RotationSpeed * Time.deltaTime);
 
             yield return Timing.WaitForOneFrame;
