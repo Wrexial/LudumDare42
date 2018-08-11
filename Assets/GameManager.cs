57fc2ca47237462a7aa42f5074135aaf1a05ee21
Unknown <dylan.abela@gmail.com>
2018-08-11 17:07:19 +0200

    Water fi bug, and edge detection kinda works..

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -4,39 +4,4 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    private const float FISH_REFRESH_RATE = 0.15f;
-    public float MinDelayForWaterChange = 0.1f;
-    public float MaxDelayForWaterChange = 0.25f;
-    public float MinWaterChange = 0.01f;
-    public float MaxWaterChange = 0.02f;
-
-    public float StartDelay = 3f;
-    public float MinDelayForWater = 2f;
-
-    public WaterHandler Water;
-    public FishHandler Fish;
-
-    private CoroutineHandle? _handleWaterCoroutine;
-
-    private void Awake()
-    {
-        _handleWaterCoroutine = Timing.RunCoroutine(HandleWater());
-    }
-
-    private void OnDestroy()
-    {
-        TimingHelpers.CleanlyKillCoroutine(ref _handleWaterCoroutine);
-    }
-
-    private IEnumerator<float> HandleWater()
-    {
-        var delay = StartDelay;
-        while (true)
-        {
-            delay -= Random.Range(MinDelayForWaterChange, MaxDelayForWaterChange);
-            delay = Mathf.Clamp(delay, MinDelayForWater, StartDelay);
-            Water.DecrementWaterLevel(Random.Range(MinWaterChange, MaxWaterChange), delay);
-            yield return Timing.WaitForSeconds(delay);
-        }
-    }
 }
--- Assets/Scripts/WaterHandler.cs
+++ Assets/Scripts/WaterHandler.cs
@@ -1,16 +1,46 @@
 ﻿using System.Collections.Generic;
 using MEC;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WaterHandler : MonoBehaviour
 {
     public float WaterSize = 500f;
     public float FaucetFillRate = 0.05f;
     public float FaucetFillSpeed = 1f;
+    public Image WaterEdge;
 
+    public float MinDelayForWaterChange = 0.1f;
+    public float MaxDelayForWaterChange = 0.25f;
+    public float MinWaterChange = 0.01f;
+    public float MaxWaterChange = 0.02f;
+
+    public float StartDelay = 3f;
+    public float MinDelayForWater = 2f;
+
+    private const float WATER_SIZE_LIMIT_MIN = 0.1f;
+    private const float WATER_SIZE_LIMIT_MAX = 1.17f;
+    private const float WATER_SIZE_LIMIT_MAX_OFF = 1f;
     private bool _faucetOn = false;
     private CoroutineHandle? _waterLevelCoroutine;
     private CoroutineHandle? _faucetControls;
+    private bool _waterEdgeOn = false;
+
+    private const float FISH_REFRESH_RATE = 0.15f;
+
+    private float _waterDelay;
+    private CoroutineHandle? _handleWaterCoroutine;
+
+    private void Awake()
+    {
+        WaterEdge.gameObject.SetActive(false);
+        _handleWaterCoroutine = Timing.RunCoroutine(HandleWater());
+    }
+
+    private void OnDestroy()
+    {
+        TimingHelpers.CleanlyKillCoroutine(ref _handleWaterCoroutine);
+    }
 
     public void TurnOnFaucet()
     {
@@ -23,9 +53,12 @@ public class WaterHandler : MonoBehaviour
     {
         _faucetOn = false;
         TimingHelpers.CleanlyKillCoroutine(ref _faucetControls);
+        TimingHelpers.CleanlyKillCoroutine(ref _handleWaterCoroutine);
+        _handleWaterCoroutine = Timing.RunCoroutine(HandleWater());
+
     }
 
-    public void DecrementWaterLevel(float value, float overTime)
+    private void DecrementWaterLevel(float value, float overTime)
     {
         if (_faucetOn)
         {
@@ -38,7 +71,7 @@ public class WaterHandler : MonoBehaviour
 
     private IEnumerator<float> HandleFaucet()
     {
-        while(true)
+        while (true)
         {
             var startScale = transform.localScale;
             var endScale = transform.localScale + (Vector3.one * FaucetFillRate);
@@ -49,8 +82,21 @@ public class WaterHandler : MonoBehaviour
                 timer += Time.deltaTime;
                 delta = Mathf.Clamp01(timer / FaucetFillSpeed);
                 transform.localScale = Vector3.Lerp(startScale, endScale, delta);
+                ClampWaterLevel();
                 yield return Timing.WaitForOneFrame;
             }
+        }
+    }
+
+    private IEnumerator<float> HandleWater()
+    {
+        _waterDelay = StartDelay;
+        while (true)
+        {
+            _waterDelay -= Random.Range(MinDelayForWaterChange, MaxDelayForWaterChange);
+            _waterDelay = Mathf.Clamp(_waterDelay, MinDelayForWater, StartDelay);
+            DecrementWaterLevel(Random.Range(MinWaterChange, MaxWaterChange), _waterDelay);
+            yield return Timing.WaitForSeconds(_waterDelay);
         }
     }
 
@@ -64,7 +110,31 @@ public class WaterHandler : MonoBehaviour
             timer += Time.deltaTime;
             delta = Mathf.Clamp01(timer / duration);
             transform.localScale = Vector3.Lerp(startScale, endScale, delta);
+            ClampWaterLevel();
             yield return Timing.WaitForOneFrame;
+        }
+    }
+
+    private void ClampWaterLevel()
+    {
+        if (transform.localScale.x > WATER_SIZE_LIMIT_MAX)
+        {
+            transform.localScale = Vector3.one * WATER_SIZE_LIMIT_MAX;
+            if (!_waterEdgeOn)
+            {
+                WaterEdge.gameObject.SetActive(true);
+                _waterEdgeOn = true;
+            }
+        }
+        else if (transform.localScale.x < WATER_SIZE_LIMIT_MIN)
+        {
+            transform.localScale = Vector3.one * WATER_SIZE_LIMIT_MIN;
+            Debug.Log("Game Over");
+        }
+        else if (_waterEdgeOn && transform.localScale.x < WATER_SIZE_LIMIT_MAX_OFF)
+        {
+            WaterEdge.gameObject.SetActive(false);
+            _waterEdgeOn = false;
         }
     }
 
@@ -72,11 +142,5 @@ public class WaterHandler : MonoBehaviour
     {
         var convertedToV2 = new Vector2(transform.localPosition.x, transform.localPosition.y);
         return (Random.insideUnitCircle * WaterSize * transform.localScale) + convertedToV2;
-    }
-
-    public void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.white;
-        Gizmos.DrawWireSphere(transform.position, WaterSize);
     }
 }
