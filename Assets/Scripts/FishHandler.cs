7c71b37597471dcb7d274e188c2012d27d295812
Unknown <dylan.abela@gmail.com>
2018-08-11 14:05:58 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/FishHandler.cs
@@ -0,0 +1,29 @@
+﻿using MEC;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishHandler : MonoBehaviour
+{
+    public float Speed;
+    private CoroutineHandle? _movingCoroutine;
+    private readonly float DISTANCE_CUTOFF = 1f;
+
+    public void MoveTo(Vector3 position)
+    {
+        TimingHelpers.CleanlyKillCoroutine(ref _movingCoroutine);
+        _movingCoroutine = Timing.RunCoroutine(HandleGoTo(position));
+    }
+
+    private IEnumerator<float> HandleGoTo(Vector3 position)
+    {
+        while (Vector3.Distance(position, transform.position) < DISTANCE_CUTOFF)
+        {
+            var step = Speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, position, step);
+            transform.LookAt(position);
+            yield return Timing.WaitForOneFrame;
+        }
+
+        _movingCoroutine = null;
+    }
+}
---
+++ Assets/Scripts/WaterHandler.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class WaterHandler : MonoBehaviour
+{
+    public void IncrementWater(float value)
+    {
+        transform.localScale += Vector3.one * value;
+    }
+
+    public Vector3 GetPointInsideWater()
+    {
+        var convertedToV2 = new Vector2(transform.localPosition.x, transform.localPosition.y);
+        return (Random.insideUnitCircle * transform.localScale) + convertedToV2;
+    }
+}
